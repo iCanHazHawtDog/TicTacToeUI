be9fd6457fbb688d47dc120c00eb34e586c2b49c
Matthew Kemnetz <kemnetz.m@gmail.com>
2011-12-04 12:59:40 -0600

    First Commit

---
+++ TicTacToeUI/src/edu/luc/tictactoe/gui/controller/MainApplication.java
@@ -0,0 +1,25 @@
+
+package edu.luc.tictactoe.gui.controller;
+
+/**
+ * @author matt
+ *
+ */
+public class MainApplication {
+
+	public static void main(String[] args) {
+		 javax.swing.SwingUtilities.invokeLater(new Runnable() {
+	            public void run() {
+	                createAndShowGUI();
+	            }
+	        });
+
+	}
+	
+	 private static void createAndShowGUI() {
+	     MainMenu matt = new MainMenu();
+	     matt.addComponentsToPane(MainMenu.frame.getContentPane());
+	     MainMenu.frame.pack();
+	     MainMenu.frame.setVisible(true);   
+	    }
+}
---
+++ TicTacToeUI/src/edu/luc/tictactoe/gui/controller/MainMenu.java
@@ -0,0 +1,73 @@
+
+package edu.luc.tictactoe.gui.controller;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+
+import edu.tictactoe.gui.resources.ImagePanel;
+
+/**
+ * @author matt
+ *
+ */
+@SuppressWarnings("serial")
+public class MainMenu extends JPanel {
+
+	static JFrame frame = new JFrame();
+	
+    public MainMenu() {
+		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 frame.setPreferredSize(new Dimension(500, 700));
+    }
+    
+    public void addComponentsToPane(Container pane) {
+//    	ImagePanel panel = new ImagePanel("/home/matt/Documents/Eclipse Workspaces/Soft Eng/TicTacToeUI/Images/UIMM.png");
+//    	pane.add(panel);
+    	pane.setLayout(new GridLayout(10,1));
+    	// Insert a space before the first button
+    	for (int i = 1; i < 6; i++ ){
+    		pane.add(new JLabel(""));
+    	}
+    	pane.add(new JButton(new AbstractAction("Play") {
+    		
+    		@Override
+        	public void actionPerformed(ActionEvent e) {
+    			frame.setVisible(false);
+    			PlayGame matt = new PlayGame();
+    			matt.addComponentsToPane(PlayGame.frame.getContentPane());
+    			PlayGame.frame.pack();
+    			PlayGame.frame.setVisible(true);
+    		}
+    	}));
+    	pane.add(new JButton(new AbstractAction("HighScores") {
+    		
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	frame.setVisible(false);
+    			HighScores matt = new HighScores();
+    			matt.addComponentsToPane(HighScores.frame.getContentPane());
+    			HighScores.frame.pack();
+    			HighScores.frame.setVisible(true);
+            }
+      	}));
+    	pane.add(new JButton(new AbstractAction("Exit") {
+    		
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	System.exit(0);
+            }
+      	}));
+    	// Insert a space after the last button
+    	pane.add(new JLabel(""));
+
+    }
+    
+}
